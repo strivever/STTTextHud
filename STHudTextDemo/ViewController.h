1e9aaf37a96285d0492c12d01060cc69eab18263
StriEver <458362366@qq.com>
2016-05-14 14:20:32 +0800

    Initial Commit

---
+++ STHudTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STHudTextDemo
+//
+//  Created by StriEver on 16/5/14.
+//  Copyright © 2016年 StriEver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ STHudTextDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  STHudTextDemo
+//
+//  Created by StriEver on 16/5/14.
+//  Copyright © 2016年 StriEver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
